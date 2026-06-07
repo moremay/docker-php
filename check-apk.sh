5db934f2d1afa78bb778fb95b6dced3b2ff188cb
moremay <root@home1.lan>
2026-06-07 19:30:33 +0800

    +edge/community

--- check-apk.sh
+++ check-apk.sh
@@ -5,13 +5,14 @@ set -e
 docker run -d --rm --name tmp-alpine --entrypoint /bin/sh alpine -c "sleep infinity"
 trap "docker stop tmp-alpine" EXIT
 
-docker exec -i tmp-alpine sh -c "
+docker exec tmp-alpine sh -c "
 echo 'https://dl-cdn.alpinelinux.org/alpine/edge/main' >> /etc/apk/repositories
+echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
 apk update
 "
 
 get_ver() {
-  docker exec -i tmp-alpine sh -c "apk query --all-matches '$1' | grep Version"
+  docker exec tmp-alpine sh -c "apk query --all-matches '$1' | grep Version"
 }
 
 for pkg in "$@"; do
