e59fd68825a224f4811531e69bb968f796225e61
moremay <root@home1.lan>
2026-07-04 13:19:35 +0800

    ⬆20260702

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -25,17 +25,17 @@ PHPIZE_DEPS_7_8="autoconf file g++ gcc libc-dev make pkgconf re2c patch dpkg-dev
 PHP_CFLAGS_5_7="-std=gnu11 -fstack-protector-strong -fpic -fpie -O2 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -Wno-discarded-qualifiers -Wno-incompatible-pointer-types -Wno-compare-distinct-pointer-types -Wno-implicit-int -Wno-implicit-function-declaration"
 PHP_CFLAGS_8="-fstack-protector-strong -fpic -fpie -O2 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
 
-export TMPL_php_ver=8.5.7
+export TMPL_php_ver=8.5.8
 export TMPL_php_url="https://www.php.net/distributions/php-${TMPL_php_ver}.tar.xz"
-export TMPL_php_sha256="01ba2ed1c2658dacf91bebc8be6a4885f69b811c7993831fc48e26107ab29985"
+export TMPL_php_sha256="58910198d19e873048fe87cdfe16bc790025417ede3d1651bfa1c4b533d573f2"
 export TMPL_PHPIZE_DEPS="$PHPIZE_DEPS_7_8"
 export TMPL_PHP_CFLAGS="$PHP_CFLAGS_8"
 export TMPL_ver=${TMPL_php_ver%.*}
 create_file $TMPL_ver
 
-export TMPL_php_ver=8.4.22
+export TMPL_php_ver=8.4.23
 export TMPL_php_url="https://www.php.net/distributions/php-${TMPL_php_ver}.tar.xz"
-export TMPL_php_sha256="696c0f6ad92e94c59059c1eb6e300842b8d050934226efcdf00f2a413cb083cf"
+export TMPL_php_sha256="1ab9f52008414e43bb2427ffa288eff2a4de39e1a830f957e800ba368d887a72"
 export TMPL_PHPIZE_DEPS="$PHPIZE_DEPS_7_8"
 export TMPL_PHP_CFLAGS="$PHP_CFLAGS_8"
 export TMPL_ver=${TMPL_php_ver%.*}
